049ec49826c5bb8b8e27a0aa687c441c600afba7
Robert <r@snow87.com>
2013-11-15 20:27:03 -0700

    gbllkjaslkdj

---
+++ TestApp/Song.cs.BASE.3332.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Song
+    {
+        public Song(String filePath)
+        {
+            TagLib.File file = TagLib.File.Create(filePath);
+        }
+    }
+}
---
+++ TestApp/Song.cs.REMOTE.3332.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Song
+    {
+        public Song(String filePath)
+        {
+            TagLib.File file = TagLib.File.Create(filePath);
+            
+        }
+    }
+}
