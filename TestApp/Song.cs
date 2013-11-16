050d1a92a8a300e106b74811dfea67ed1ee3ea2e
Robert <r@snow87.com>
2013-11-15 19:46:32 -0700

    song stuff

--- TestApp/Song.cs
+++ TestApp/Song.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TestApp
 {
     class Song
     {
+        public Song(String filePath)
+        {
+            TagLib.File file = TagLib.File.Create(filePath);
+        }
     }
 }
